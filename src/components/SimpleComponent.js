b66053acfa6c38dfe0eafd8a08f52c072c06d5af
Nkosi Enciso Givhan <codeatblack@gmail.com>
2018-11-20 17:01:15 -0800

    Making functional and regular components

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,26 @@
 // Code SimpleComponent Here
+import React { Component } from 'react'
+
+class SimpleComponent extends Component {
+  constructor() {
+    this.state ={
+      mood: "happy"
+    }
+  }
+
+  handleClick = (event) => {
+    if (this.state.mood === "happy"){
+      this.setState({mood: "sad"})
+    } else {
+      this.setState({mood: "happy"})
+    }
+  }
+
+  render(){
+    return (
+      <div handleClick={this.handleClick}>
+        {this.state.mood}
+      </div>
+    )
+  }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,8 @@
 // Code SimplerComponent Here
+import React { Component } from 'react'
+
+function SimplerComponent extends Component (props)=> (
+  <div>
+    {this.props.mood}
+  </div>
+)
